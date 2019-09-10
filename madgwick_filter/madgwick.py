640e0a1c5e46f15c907b7d3926938bd5867873ba
abhi1625 <abhi1625@terpmail.umd.edu>
2019-09-10 01:44:24 -0400

    PLotting and preprocessing updated

--- madgwick_filter/madgwick.py
+++ madgwick_filter/madgwick.py
@@ -151,25 +151,31 @@ class madgwick:
 def process_data(acc_data,acc_scale,acc_bias,gyro):
 
     acc_data = acc_data + acc_bias[:,np.newaxis]
-    #acc_scale = np.reciprocal(acc_scale)
+    acc_scale = np.reciprocal(acc_scale)
     print acc_data[:,0]
-    acc_data[0] = np.multiply(acc_scale[0],acc_data[0])
-    acc_data[1] = np.multiply(acc_scale[1],acc_data[1])
-    acc_data[2] = np.multiply(acc_scale[2],acc_data[2])
+    acc_data[0] = np.multiply(acc_scale[0],acc_data[0])/9.8
+    acc_data[1] = np.multiply(acc_scale[1],acc_data[1])/9.8
+    acc_data[2] = np.multiply(acc_scale[2],acc_data[2])/9.8
     # print acc_data[:,0]
     # input('a')
-    gyro_data = 3300/1023*np.pi/180*0.3*gyro[:]
+    gyro_data = (3300/1023)*(np.pi/180)*0.3*gyro[[2,0,1],:]
     # print gyro_data.shape
     # input('a')
     return acc_data, gyro_data
 
+def rot2eul(R):
+    beta = -np.arcsin(R[2,0])
+    alpha = np.arctan2(R[2,1]/np.cos(beta),R[2,2]/np.cos(beta))
+    gamma = np.arctan2(R[1,0]/np.cos(beta),R[0,0]/np.cos(beta))
+    return np.array((alpha, beta, gamma))
+                
 
 
 imu_params = scipy.io.loadmat('./IMUParams.mat')
 acc_scale = imu_params['IMUParams'][0]
 acc_bias = imu_params['IMUParams'][1]
-#data = scipy.io.loadmat('/home/abhinav/Gits/drone-course/madgwick_filter/YourDirectoryID_p1a/Data/Train/IMU/imuRaw6.mat')
-data = scipy.io.loadmat('./ExampleData.mat')
+data = scipy.io.loadmat('/home/abhinav/Gits/drone-course/madgwick_filter/YourDirectoryID_p1a/Data/Train/IMU/imuRaw1.mat')
+
 acc = data['vals'][:3,:]
 print acc.shape
 gyro = data['vals'][-3:,:]
@@ -177,7 +183,6 @@ print gyro.shape
 input('a')
 acc_all,gyro_all = process_data(acc,acc_scale,acc_bias,gyro)
 
-imu_data_vicon = scipy.io.loadmat('/home/abhinav/Gits/drone-course/madgwick_filter/YourDirectoryID_p1a/Data/Train/Vicon/viconRot6.mat')
 
 # thresh = len(imu_data_orig['Accelerometer'])-1
 # print(thresh)
@@ -187,7 +192,7 @@ q_est = Quaternion(1,0,0,0)
 q_init = Quaternion(1,0,0,0)
 angles = np.array([0,0,0])
 mk=madgwick(q = q_init)
-thresh = len(acc_all)-1
+thresh = acc_all.shape[1] -1
 print(thresh)
 # thresh = 100
 i=0
@@ -195,7 +200,8 @@ employee_file = open('my_madwick.csv', mode='w')
 quat_file  = open('quat_my_madwick.csv', mode='w')
 employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 quat_writer = csv.writer(quat_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-input('aa')
+# input('aa')
+print(acc_all.shape)
 while i<=thresh:
     
 #     print i+1
@@ -222,9 +228,11 @@ while i<=thresh:
     temp = np.array([phi, thetha, psy])
 
     angles= np.vstack((angles,temp))
+
 #     print "q = ",q
     q_est = Quaternion(q[0],q[1],q[2],q[3])
     i+=1
+
 angles = angles[1:]
 # print mk.return_num_ignored_samples
 # num = mk.return_num_ignored_samples
@@ -232,101 +240,76 @@ angles = angles[1:]
 num=0
 acc_all = acc_all[num:]
 gyro_all = gyro_all[num:]
+
+#PLot vicon data
+imu_data_vicon = scipy.io.loadmat('/home/abhinav/Gits/drone-course/madgwick_filter/YourDirectoryID_p1a/Data/Train/Vicon/viconRot1.mat')
+rots = imu_data_vicon['rots']
+vicon_data = np.zeros((3,rots.shape[2]),dtype=np.float32)
+for ang in range(rots.shape[2]):
+    vicon_data[:,ang] = rot2eul(rots[:,:,ang])
+
+print(vicon_data.shape)
+t_vicon = np.linspace(1,vicon_data.shape[1],num=vicon_data.shape[1])
+input('aaaa')
+    
 t = np.linspace(1,int(acc_all.shape[0]),num = int(acc_all.shape[0]))
 t_angles = np.linspace(1,int(angles.shape[0]),num = int(angles.shape[0]))
 fig=plt.figure(1)
 # plt.rcParams['figure.figsize'] = [10,10]
-fig2=plt.figure(2)
-# plt.rcParams['figure.figsize'] = [10,10]
-plt.figure(figsize=(20,20))
-a1 = plt.subplot(3,3,1)
+
+a1 = plt.subplot(3,4,1)
 plt.plot(t[:thresh],acc_all[:thresh,0],'r-')
 a1.title.set_text('X-accel')
 
-a2 = plt.subplot(3,3,2)
+a2 = plt.subplot(3,4,2)
 plt.plot(t[:thresh],gyro_all[:thresh,0],'r-')
 a2.title.set_text('X-gyro')
 
 # print t.shape,angles[:,0].shape
 
-a3 = plt.subplot(3,3,3)
+a3 = plt.subplot(3,4,3)
 plt.plot(t_angles,angles[:,0],'r-')
 a3.title.set_text('X-madgwick')
 
+a10 = plt.subplot(3,4,4)
+plt.plot(t_vicon,vicon_data[0,:],'r-')
+a10.title.set_text('X-madgwick')
 
-a4 = plt.subplot(3,3,4)
+a4 = plt.subplot(3,4,5)
 plt.plot(t[:thresh],acc_all[:thresh,1],'g-')
 a4.title.set_text('Y-accel')
 
-a5 = plt.subplot(3,3,5)
+a5 = plt.subplot(3,4,6)
 plt.plot(t[:thresh],gyro_all[:thresh,1],'g-')
 a5.title.set_text('Y-gyro')
 
-a6 = plt.subplot(3,3,6)
+a6 = plt.subplot(3,4,7)
 plt.plot(t_angles,angles[:,1],'g-')
 a6.title.set_text('Y-madgwick')
 
-a7 = plt.subplot(3,3,7)
+a11 = plt.subplot(3,4,8)
+plt.plot(t_vicon,vicon_data[1,:],'g-')
+a11.title.set_text('Y-madgwick')
+
+a7 = plt.subplot(3,4,9)
 plt.plot(t[:thresh],acc_all[:thresh,2],'b-')
 a7.title.set_text('Z-accel')
 
-a8 = plt.subplot(3,3,8)
+a8 = plt.subplot(3,4,10)
 plt.plot(t[:thresh],gyro_all[:thresh,2],'b-')
 a8.title.set_text('Z-gyro')
 
-a9 = plt.subplot(3,3,9)
+a9 = plt.subplot(3,4,11)
 plt.plot(t_angles,angles[:,2],'b-')
 a9.title.set_text('Z-madgwick')
 
+a12 = plt.subplot(3,4,12)
+plt.plot(t_vicon,vicon_data[2,:],'b-')
+a12.title.set_text('Z-madgwick')
 
 print(len(angles))
 
 
-plt.close
-# acc_all = imu_data_orig['Accelerometer']
-# gyro_all = imu_data_orig['Gyroscope']
-t = np.linspace(1,int(acc_all.shape[0]),num = int(acc_all.shape[0]))
-
-fig2=plt.figure(2)
-# plt.rcParams['figure.figsize'] = [10,10]
-plt.figure(figsize=(20,20))
-a1 = plt.subplot(3,3,1)
-plt.plot(t,acc_all[:,0],'r-')
-a1.title.set_text('X-accel')
-
-a2 = plt.subplot(3,3,2)
-plt.plot(t,gyro_all[:,0],'r-')
-a2.title.set_text('X-gyro')
-
-a3 = plt.subplot(3,3,4)
-plt.plot(t,acc_all[:,1],'g-')
-a3.title.set_text('Y-accel')
-
-a4 = plt.subplot(3,3,5)
-plt.plot(t,gyro_all[:,1],'g-')
-a4.title.set_text('Y-gyro')
-
-a5 = plt.subplot(3,3,7)
-plt.plot(t,acc_all[:,2],'b-')
-a5.title.set_text('Z-accel')
-
-a6 = plt.subplot(3,3,8)
-plt.plot(t,gyro_all[:,2],'b-')
-a6.title.set_text('Z-gyro')
-
-a6 = plt.subplot(3,3,8)
-plt.plot(t,gyro_all[:,2],'b-')
-a6.title.set_text('Z-gyro')
 plt.show()
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
