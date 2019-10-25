625b08051da4e96ed3491155a67ab8f397fb9a0f
Chahat Deep <chahatdeep>
2019-10-25 17:03:07 -0400

    controller bug fixed

--- Window_detection/trajectory_tracking.py
+++ Window_detection/trajectory_tracking.py
@@ -203,10 +203,11 @@ class trajectory_track:
 		self.current_state = np.zeros((6,))
 		self.prev_vel_odom = np.zeros((2,1))
 		self.curr_vel_odom = np.zeros((2,1))
+		self.curr_vel_yaw = 0.0 
 		# current and previous position in x-y
 		self.next_des = np.zeros((2,))
 		self.prev_des = np.zeros((2,))
-
+		
 		# states for trajectory tracking
 		self.pos_perpendicular = np.zeros((2,1))
 		self.vel_perpendicular = np.zeros((2,1))
@@ -218,8 +219,8 @@ class trajectory_track:
 		# self.pose = data.pose.pose
 		# print "pose ", self.pose
 		# self.Twist = data.twist.twist
-		self.current_state[0] = data.pose.pose.position.x
-		self.current_state[1] = data.pose.pose.position.y
+		self.current_state[0] = data.twist.twist.linear.x*0.2 + self.current_state[0]
+		self.current_state[1] = data.twist.twist.linear.y*0.2 + self.current_state[1]
 		self.current_state[2] = data.pose.pose.position.z
 		self.current_state[3] = data.pose.pose.orientation.x
 		self.current_state[4] = data.pose.pose.orientation.y
@@ -227,6 +228,7 @@ class trajectory_track:
 
 		self.curr_vel_odom[0] = data.twist.twist.linear.x
 		self.curr_vel_odom[1] = data.twist.twist.linear.y 
+		self.curr_vel_yaw = data.twist.twist.angular.z
 		# self.odom_data = data
 
 	def takeoff(self):
@@ -258,7 +260,10 @@ class trajectory_track:
 		pos = np.array([[self.current_state[0] - self.prev_des[0]],
 						[self.current_state[1] - self.prev_des[1]]])
 		self.pos_perpendicular = np.matmul(l_perpendicular.T, pos)
-
+		pos_dist = np.array([[self.next_des[0] - self.current_state[0]],
+				     [self.next_des[0] - self.current_state[1]]])
+		pos_parallel = np.matmul(l_parallel.T, pos_dist)
+		print("distance to dest: ",pos_parallel)
 
 	def compute_ctrl_inputs(self, l_parallel, l_perpendicular):
 		K_line = np.array([[0.2207, 0.2634, 	 0, 	0],
@@ -271,15 +276,26 @@ class trajectory_track:
 		states = states.reshape(4,1)
 		ctrl_inputs = - np.matmul(K_line,states)
 		# print(l_perpendicular.shape)
-		ctrl_inputs_gf = l_parallel*ctrl_inputs[1] + l_perpendicular*ctrl_inputs[0]
+		# ctrl_inputs_gf = l_parallel*ctrl_inputs[0] + l_perpendicular*ctrl_inputs[1]
+		ctrl_inputs_gf = np.zeros((2,))
+		gains = np.array([[0.2236, 0.2657]])
+
+		print(gains)
+		x_pos = np.array([[self.current_state[0] - self.next_des[0]],
+				  self.curr_vel_odom[0]])
+		print(x_pos)
+		y_pos = np.array([[self.current_state[1] - self.next_des[1]],
+				  self.curr_vel_odom[1]])
+		ctrl_inputs_gf[0] = - np.matmul(gains, x_pos)
+		ctrl_inputs_gf[1] = - np.matmul(gains, y_pos)
 		return ctrl_inputs_gf
 
 
 def main():
 	rospy.init_node('trajectory_following', anonymous=True)
 	track_ob = trajectory_track()
-	# track_ob.takeoff()
-	# rospy.sleep(2)
+	track_ob.takeoff()
+	rospy.sleep(5)
 	init_flag = True
 	rate = rospy.Rate(10)
 	while (not rospy.is_shutdown()):
@@ -292,28 +308,27 @@ def main():
 			bias_ang_z = track_ob.current_state[5]
 
 			init_flag = False
-		track_ob.current_state = track_ob.current_state - np.array([bias_x,
-																	bias_y,
-																	bias_z,
-																	bias_ang_x,
-																	bias_ang_y,
-																	bias_ang_z]) 
-		print(track_ob.current_state[5])
-		# print(track_ob.current_state)
-		track_ob.next_des = np.array([1,1])
+		#track_ob.current_state = track_ob.current_state - np.array([bias_x,bias_y,bias_z,bias_ang_x,bias_ang_y,bias_ang_z]) 
+		print("current x:",track_ob.current_state[0])
+		print("current y:",track_ob.current_state[1])
+		#) print(track_ob.current_state)
+		track_ob.next_des = np.array([1,0])
 		l_parallel, l_perpendicular = track_ob.traj_gen()
 		yaw_reference = math.atan2(l_parallel[1], l_parallel[0])
+		#print("yaw reference",yaw_reference)
 		track_ob.compute_states(l_parallel, l_perpendicular)	
 		ctrl_inputs = track_ob.compute_ctrl_inputs(l_parallel,l_perpendicular)
 		vel = Twist()
-		vel.linear.x = 0.1*ctrl_inputs[0]
-		vel.linear.y = 0.1*ctrl_inputs[1]
+		vel.linear.x = 1*ctrl_inputs[0]
+		vel.linear.y = 1*ctrl_inputs[1]
 		vel.linear.z = 0
 			
 		vel.angular.x = 0
 		vel.angular.y = 0
-		vel.angular.z = 0.1*(yaw_reference - track_ob.current_state[5])
-		print(vel.angular.z)
+		#vel.angular.z = 0.1*(yaw_reference - track_ob.current_state[5]) - 0.01*track_ob.curr_vel_yaw 
+		vel.angular.z = 0
+		print("vel x",vel.linear.x )
+		print("vel y", vel.linear.y)
 		track_ob.bebop_vel_pub.publish(vel)
 		rate.sleep()
 
